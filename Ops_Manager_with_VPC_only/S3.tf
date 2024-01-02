bdd92b1e7c1604e24505b50f58b1add26f2b00da
ATornike <102183575+ATornike@users.noreply.github.com>
2024-01-02 15:05:33 -0500

    Create S3.tf

---
+++ Ops_Manager_with_VPC_only/S3.tf
@@ -0,0 +1,13 @@
+variable "s3_buckets" {
+  // The buckets are manually be added to the IAM Policy!! 
+  description = "Creates S3 buckets needed for TAS"
+  type        = list(string)
+  default     = ["tas-ops-manager-bucket-123", "tas-buildpacks-bucket-123", "tas-packages-bucket-123", "tas-resources-bucket-123", "tas-droplets-bucket-123"]
+}
+
+
+resource "aws_s3_bucket" "tas_s3_buckets" {
+  count = length(var.s3_buckets)
+  //bucket = [count.index]
+  bucket = var.s3_buckets[count.index]
+}
